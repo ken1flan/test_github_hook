77a59265775b1fa3a9dd63de76bfee6471317770
ken1flan <ken1flan@gmail.com>
2015-06-09 19:15:51 +0900

    gitコマンドを実行するようにした

--- myapp.rb
+++ myapp.rb
@@ -7,13 +7,13 @@ post "/github" do
   repository_root = ENV["REPOSITORY_ROOT"].to_s
   git_options = "--git-dir=#{repository_root}/#{repository_name}/.git"
 
-  # if system("pgrep -f \"sh -c git #{git_options}\"")
-  #   next "Found another process for #{repository_name)"
-  # end
+  if system("pgrep -f \"sh -c git #{git_options}\"")
+    next "Found another process for #{repository_name}"
+  end
 
   cmd = "git #{git_options} remote prune origin" +
         " && git #{git_options} fetch origin"
-  # pid = Process.spawn(cmd)
-  # Process.detach(pid)
+  pid = Process.spawn(cmd)
+  Process.detach(pid)
   "Spawn following command: #{cmd}"
 end
