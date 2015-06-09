0a9cec8853d8effa14526b200fa83c431f6e5ccd
ken1flan <ken1flan@gmail.com>
2015-06-09 11:17:56 +0900

    payloadをlogging

--- myapp.rb
+++ myapp.rb
@@ -3,10 +3,15 @@ $:.unshift File.dirname(__FILE__)
 require "sinatra"
 require "github"
 
+configure :production, :development do
+  enable :logging
+end
+
 get "/" do
   "hello, world!"
 end
 
 post "/github" do
   Github::hello(params)
+  logger.info "payload:" + params[:payload].to_s
 end
